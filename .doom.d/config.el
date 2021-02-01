7f2eb66262e899c336f6fc5c9b0e45f29ce26372
jory18 <ksui21@yahoo.co.jp>
2021-02-01 18:15:20 +0100

    arst

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,4 +1,5 @@
 
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/org/")
@@ -12,7 +13,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type 'nil)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -36,6 +37,9 @@
 (evil-global-set-key 'normal "K" 'evil-replace-state)
 (evil-global-set-key 'motion "l" 'evil-forward-word-end)
 (evil-global-set-key 'motion "L" 'evil-forward-WORD-end)
+
+(map! :leader
+      :desc "Recent files" "r" #'counsel-recentf)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -76,7 +76,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -146,10 +146,10 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org               ; organize your plain life in plain text
-        +pretty
-        +roam
-        +journal):
+       ;;(org               ; organize your plain life in plain text
+       ;; +pretty
+       ;; +roam
+       ;; +journal):
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
